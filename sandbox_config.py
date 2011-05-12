372de7f35dac005382edb411a4bccd82e0e9761b
Ceasar Bautista <ceasarb@seas.upenn.edu>
2011-05-12 16:09:53 -0400

    added config

---
+++ sandbox_config.py
@@ -0,0 +1,11 @@
+"""
+NOTE: this is a development-specific file that lets us set various variables
+(such as branch name, etc) so that we can share changes in the more general
+django.wsgi and settings.py files without fear. This should be configured per-
+sandbox. This and nothing else.
+Author: AMK, Dec 27, 2010"""
+import os
+
+DISPLAY_NAME = "studyspaces-ceasarb" # IE, pennapps.com/display_name
+DEV_ROOT = "/home/ceasarb/ceasarb_dev" # root of all apps you're working on
+STUDYSPACES_APP_ROOT = os.path.join(DEV_ROOT, "studyspaces")
