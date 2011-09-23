30bfd2a69d0157df2c3c3b42a3efb4462e35da15
Nop Jiarathanakul <pjia@seas.upenn.edu>
2011-09-23 00:05:28 -0400

    implemented initial localStorage for toggling view between Average and Recent modes, no functionality yet, only button switching, but is now persistent.

--- media/front-end/coffee/common.coffee
+++ media/front-end/coffee/common.coffee
@@ -3,3 +3,22 @@ window.toggle_row = (row_id) ->
   
 window.toggle_choose_cols = () ->
   $("#choose-cols").toggle()
+  
+# 0=average (default), 1=recent
+window.toggle_view = (view_id) ->
+  if "#{view_id}" == "0"
+    $("a#view_average").addClass("disabled")
+    $("a#view_recent").removeClass("disabled")
+  else
+    $("a#view_average").removeClass("disabled")
+    $("a#view_recent").addClass("disabled")
+  localStorage["pcr_view"] = view_id
+  
+###
+DOCUMENT READY
+###
+$(document).ready ->
+  if not localStorage["pcr_view"]?
+    localStorage["pcr_view"] = "0"
+  else
+    toggle_view(localStorage["pcr_view"])
--- media/front-end/js/common.js
+++ media/front-end/js/common.js
@@ -5,4 +5,24 @@
   window.toggle_choose_cols = function() {
     return $("#choose-cols").toggle();
   };
+  window.toggle_view = function(view_id) {
+    if (("" + view_id) === "0") {
+      $("a#view_average").addClass("disabled");
+      $("a#view_recent").removeClass("disabled");
+    } else {
+      $("a#view_average").removeClass("disabled");
+      $("a#view_recent").addClass("disabled");
+    }
+    return localStorage["pcr_view"] = view_id;
+  };
+  /*
+  DOCUMENT READY
+  */
+  $(document).ready(function() {
+    if (!(localStorage["pcr_view"] != null)) {
+      return localStorage["pcr_view"] = "0";
+    } else {
+      return toggle_view(localStorage["pcr_view"]);
+    }
+  });
 }).call(this);
