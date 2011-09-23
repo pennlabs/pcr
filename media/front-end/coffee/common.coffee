ff8be1262ee69de5baf468a29680b25a80327db9
Nop Jiarathanakul <pjia@seas.upenn.edu>
2011-09-23 01:00:58 -0400

    localStorage for choosecols. no functionality yet. just storing and checked boxes.

--- media/front-end/coffee/common.coffee
+++ media/front-end/coffee/common.coffee
@@ -3,6 +3,18 @@ window.toggle_row = (row_id) ->
   
 window.toggle_choose_cols = () ->
   $("#choose-cols").toggle()
+
+window.submit_choose_cols = () ->
+# TODO: more efficient method please???
+  str = ""
+  for i in [0..10]
+    str +=
+    if $("#choose-cols input[name='choosecols#{i}']")
+    .attr('checked')=="checked"
+    then 1 else 0
+    
+  localStorage["pcr_choosecols"] = str
+  toggle_choose_cols()
   
 # 0=average (default), 1=recent
 window.toggle_view = (view_id) ->
@@ -18,7 +30,22 @@ window.toggle_view = (view_id) ->
 DOCUMENT READY
 ###
 $(document).ready ->
+
+  ### setup view mode ###
   if not localStorage["pcr_view"]?
     localStorage["pcr_view"] = "0"
   else
     toggle_view(localStorage["pcr_view"])
+  
+  ### setup choose columns ###
+  if localStorage["pcr_choosecols"]?
+    str = localStorage["pcr_choosecols"]
+  
+    for i in [0..10]
+      $("#choose-cols input[name='choosecols#{i}']")
+        .attr('checked', str.charAt(i)=="1")
+  else
+    for i in [0..2]
+      $("#choose-cols input[name='choosecols#{i}']")
+        .attr('checked', true)
+    localStorage["pcr_choosecols"] = "11100000000"
--- media/front-end/js/common.js
+++ media/front-end/js/common.js
@@ -5,6 +5,15 @@
   window.toggle_choose_cols = function() {
     return $("#choose-cols").toggle();
   };
+  window.submit_choose_cols = function() {
+    var i, str;
+    str = "";
+    for (i = 0; i <= 10; i++) {
+      str += $("#choose-cols input[name='choosecols" + i + "']").attr('checked') === "checked" ? 1 : 0;
+    }
+    localStorage["pcr_choosecols"] = str;
+    return toggle_choose_cols();
+  };
   window.toggle_view = function(view_id) {
     if (("" + view_id) === "0") {
       $("a#view_average").addClass("disabled");
@@ -19,10 +28,26 @@
   DOCUMENT READY
   */
   $(document).ready(function() {
+    /* setup view mode */
+    var i, str, _results;
     if (!(localStorage["pcr_view"] != null)) {
-      return localStorage["pcr_view"] = "0";
+      localStorage["pcr_view"] = "0";
     } else {
-      return toggle_view(localStorage["pcr_view"]);
+      toggle_view(localStorage["pcr_view"]);
+    }
+    /* setup choose columns */
+    if (localStorage["pcr_choosecols"] != null) {
+      str = localStorage["pcr_choosecols"];
+      _results = [];
+      for (i = 0; i <= 10; i++) {
+        _results.push($("#choose-cols input[name='choosecols" + i + "']").attr('checked', str.charAt(i) === "1"));
+      }
+      return _results;
+    } else {
+      for (i = 0; i <= 2; i++) {
+        $("#choose-cols input[name='choosecols" + i + "']").attr('checked', true);
+      }
+      return localStorage["pcr_choosecols"] = "11100000000";
     }
   });
 }).call(this);
