021f16f96215de1e2f2027dfdf62d0e996517a83
Alexey Komissarouk <AlexeyMK@gmail.com>
2011-11-17 22:44:06 -0500

    fixed ceasar's average bug

--- app/average.py
+++ app/average.py
@@ -3,7 +3,6 @@ from __future__ import division
 
 ERROR = u"\u00A0" #empty string
 
-
 def average(reviews, attr):
   average = 0.0
   valid = 0
@@ -15,7 +14,7 @@ def average(reviews, attr):
     else:
       valid += 1
   if valid > 0:
-    return "%.2f" % average / valid
+    return "%.2f" % (average / valid)
   else:
     return ERROR
 
