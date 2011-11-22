39ddd67fc93ca938b4644d3a870ecd124329162b
Ceasar Bautista <ceasarb@seas.upenn.edu>
2011-11-21 20:43:25 -0500

    Split root urlconf into app-specific urls confs.

--- urls.py
+++ urls.py
@@ -4,8 +4,7 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     (r'^$', direct_to_template, {'template': 'index.html'}),
-    (r'autocomplete_data.json', 'apps.searchbar.views.autocomplete_data'),
-    (r'(about)', 'apps.static.views.static'),
-    (r'(faq)', 'apps.static.views.static'),
     (r'^', include('apps.pcr_detail.urls')),
+    (r'^', include('apps.searchbar.urls')),
+    (r'^', include('apps.static.urls')),
 )
