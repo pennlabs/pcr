f7fbb4d42902bebe2451537adf762014b33ac4f8
Ceasar Bautista <ceasarb@seas.upenn.edu>
2011-12-16 19:44:07 -0500

    Removed dataflow, which uses threads to (presumably) speed up API calls. This solves the error of 500 errors being created instead of 404 errors due to exceptions being thrown in dataflow threads. Furthermore, I'm not sure this actually slows things down any, as I added it based on ancedotal evidence and later with some non-rigorous testing determined it wasn't really helping anyway.

--- lib/api/api.py
+++ lib/api/api.py
@@ -4,12 +4,10 @@ import urllib2
 import json
 
 from pcrsite.sandbox_config import DOMAIN, TOKEN
-from dataflow import dataflow
 from memoize import memoize
 
 
 @memoize
-@dataflow
 def api(domain, *args, **kwargs):
   path = "".join((domain, "/".join([str(arg) for arg in args]), "?", urllib.urlencode(kwargs)))
   try:
