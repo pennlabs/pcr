1d9c9758cbd4bad20461946dae9626cf50bf0788
Greg Terrono <gterrono@seas.upenn.edu>
2012-02-02 23:02:03 -0500

    Got the server to send only the relevent information

--- media/front-end/coffee/index.coffee
+++ media/front-end/coffee/index.coffee
@@ -3,11 +3,7 @@ DOCUMENT READY
 ###
 $(document).ready ->
   callback = () ->
-    $("#search-loading").hide()
-    $("#search-container").fadeIn(1000, () ->
-      $("#searchbox").focus()
-    )
-    alert("data loaded")
+    $("#searchbox").autocomplete("search")
   $("#searchbox").keypress( () ->
     if $("#searchbox").val().length == 2
       init_search_box("", callback, $("#searchbox").val())
--- media/front-end/js/index.js
+++ media/front-end/js/index.js
@@ -4,11 +4,7 @@
   */  $(document).ready(function() {
     var callback;
     callback = function() {
-      $("#search-loading").hide();
-      $("#search-container").fadeIn(1000, function() {
-        return $("#searchbox").focus();
-      });
-      return alert("data loaded");
+      return $("#searchbox").autocomplete("search");
     };
     $("#searchbox").keypress(function() {
       if ($("#searchbox").val().length === 2) {
