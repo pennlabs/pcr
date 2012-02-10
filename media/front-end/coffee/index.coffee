eccc29862b895cb875ca29e6ccd492abe4a4790e
Greg Terrono <gterrono@seas.upenn.edu>
2012-02-09 22:05:24 -0500

    Fixed index.coffee to display the data after the correct number of letters

--- media/front-end/coffee/index.coffee
+++ media/front-end/coffee/index.coffee
@@ -3,10 +3,13 @@ DOCUMENT READY
 ###
 $(document).ready ->
   callback = () ->
+    $("#searchbox").autocomplete("enable")
     $("#searchbox").autocomplete("search")
-  $("#searchbox").keypress( () ->
+  $("#searchbox").keypress( () -> setTimeout(() ->
     if $("#searchbox").val().length == 2
       init_search_box("", callback, $("#searchbox").val())
-  )
+    else if $("#searchbox").val().length < 2
+      $("#searchbox").autocomplete("disable")
+  , 0))
   $("#search-loading").hide()
   $("#search-container").show()
--- media/front-end/js/index.js
+++ media/front-end/js/index.js
@@ -4,12 +4,17 @@
   */  $(document).ready(function() {
     var callback;
     callback = function() {
+      $("#searchbox").autocomplete("enable");
       return $("#searchbox").autocomplete("search");
     };
     $("#searchbox").keypress(function() {
-      if ($("#searchbox").val().length === 2) {
-        return init_search_box("", callback, $("#searchbox").val());
-      }
+      return setTimeout(function() {
+        if ($("#searchbox").val().length === 2) {
+          return init_search_box("", callback, $("#searchbox").val());
+        } else if ($("#searchbox").val().length < 2) {
+          return $("#searchbox").autocomplete("disable");
+        }
+      }, 0);
     });
     $("#search-loading").hide();
     return $("#search-container").show();
