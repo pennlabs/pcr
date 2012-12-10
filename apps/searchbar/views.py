f6b9671139862c092c4454a0e418974f6aa78164
Kyle Hardgrave <kyle@kylehardgrave.com>
2012-12-10 15:31:12 -0500

    Including _all_ autocomplete data if no start string is provided

--- apps/searchbar/views.py
+++ apps/searchbar/views.py
@@ -1,4 +1,4 @@
-import json
+3import json
 
 from django.http import HttpResponse
 
@@ -9,6 +9,9 @@ def json_response(result_dict):
   return HttpResponse(content=json.dumps(result_dict))
 
 def autocomplete_data(request, start=None):
+  if start:
+    start = start.lower()
+
   #1. Hit API up for course-history data, push into nop's desired format
   def alias_to_code(alias, sep="-"):
     code, num = alias.split('-')
@@ -21,36 +24,38 @@ def autocomplete_data(request, start=None):
                                   for sep in ['', '-', ' ']]
                                  + [course['name'].lower()])
       course_title = alias_to_code(alias, ' ')
-      if start in course_title or start in course_keywords:
+      if not start or start in course_keywords:
         courses.append({"category": "Courses",
                 "title": course_title,
                 "desc": course['name'],
                 "url": "course/" + alias_to_code(alias),
                 "keywords": course_keywords
                 })
-              
+
   #2. Hit API up for instructor data, push into nop's desired format
-  instructors_from_api = api('instructors')['values']  
+  instructors_from_api = api('instructors')['values']
   instructors=[{"category": "Instructors",
                 "title": instructor['name'],
                 "desc": ", ".join(instructor['depts']),
                 "url": "instructor/" + instructor['id'],
                 "keywords": instructor['name'].lower()
-               } for instructor in instructors_from_api 
-                 if 'depts' in instructor and (start in instructor['name']
-                   or start in instructor['name'].lower())]
+                }
+               for instructor in instructors_from_api
+               if ('depts' in instructor and
+                   (start in instructor['name'].lower() or not start))]
+
 
   #3. Hit API up for department data, push into nop's desired format
-  departments_from_api = api('depts')['values']  
+  departments_from_api = api('depts')['values']
   departments=[{"category": "Departments",
                 "title": department['id'],
                 "desc": "".join(department['name']),
                 "url": "department/" + department['id'],
                 "keywords": department['name']
                } for department in departments_from_api
-                 if start in department['id'].lower() 
+                 if not start or start in department['id'].lower()
                    or start in department['name'].lower()]
 
   #4. Respond in JSON
-  return json_response({"courses":courses, "instructors":instructors, 
+  return json_response({"courses":courses, "instructors":instructors,
     "departments":departments})
