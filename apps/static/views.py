a216dcec6ec26e7b085e3183336d2f6d1e5e3730
VivekPanyam <viv.panyam@gmail.com>
2013-04-18 16:38:48 -0300

    Added API Proxy

--- apps/static/views.py
+++ apps/static/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
+import mimetypes
+import urllib2
 
 from pcrsite.lib.api import api
 
@@ -9,3 +12,18 @@ def static(request, page):
     'content': api('pcrsite-static', page)
     }
   return render_to_response('static.html', context)
+
+
+def proxy_to(request, path, target_url):
+    url = '%s%s' % (target_url, path)
+    if request.META.has_key('QUERY_STRING'):
+        url += '?' + request.META['QUERY_STRING']
+    try:
+        proxied_request = urllib2.urlopen(url)
+        status_code = proxied_request.code
+        mimetype = proxied_request.headers.typeheader or mimetypes.guess_type(url)
+        content = proxied_request.read()
+    except urllib2.HTTPError as e:
+        return HttpResponse(e.msg, status=e.code, mimetype='text/plain')
+    else:
+        return HttpResponse(content, status=status_code, mimetype=mimetype)
