26d639da2e47750c72c0dbb0636e080c58b04768
VivekPanyam <viv.panyam@gmail.com>
2013-04-18 16:45:38 -0300

    Update urls.py

--- apps/static/urls.py
+++ apps/static/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'(about)', 'apps.static.views.static'),
     (r'(faq)', 'apps.static.views.static'),
-    (r'^chrome/api/(?P<path>.*)$', 'apps.static.views.proxy'),
-
+    (r'^chrome/api/(?P<path>.*)$', 'apps.static.views.proxy')
 )
 
