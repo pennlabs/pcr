103802a77b9db2af001c9ab0c74838a0478e9d12
VivekPanyam <viv.panyam@gmail.com>
2013-04-18 16:40:37 -0300

    Added API Proxy

--- apps/static/urls.py
+++ apps/static/urls.py
@@ -4,5 +4,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'(about)', 'apps.static.views.static'),
     (r'(faq)', 'apps.static.views.static'),
+    (r'^chrome/api/(?P<path>.*)$', 'apps.static.views.proxy'),
+
 )
 
