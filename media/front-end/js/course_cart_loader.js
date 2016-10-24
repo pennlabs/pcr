aa3a90d9b4bf4d5a2a9f7ee468cd6b8e737bdabe
Adam Domingoes <adamdom@wharton.upenn.edu>
2016-10-24 14:10:35 -0400

    Remove from cart option

--- media/front-end/js/course_cart_loader.js
+++ media/front-end/js/course_cart_loader.js
@@ -26,36 +26,57 @@ setTimeout(function(){
 			});
 		return {course: title, professor: instructor, info: data};
 	}
+	var listProfessors = function() {
+		var list = "<div id='divList'> <ul class='professorList'>";
+		var close = "$('[data-original-title]').popover('hide');";
+		for (var i = 0; i < instructors.length; i++) {
+			list += "<li><button id='" + instructors[i].split(" ").join("") + "' onclick='addToCourseCart(instructors[" + i  +"]);'>" + instructors[i]  + "</button></li>";
+		}
+		list += "</ul></div>";
+		$("#popup").attr("data-content",list);
+	}
+	
+	var addCartButton = function() {
+		$("p.title")[0].innerHTML = $("p.title")[0].innerHTML + "<p class='courseCart'> <small id='popup' data-html='true' data-toggle='popover' data-content='' title='Select Professor'><i class='fa fa-plus-square-o' aria-hidden='true'></i> Add to My Cart</small></p>";
+		listProfessors();
+		$("[data-toggle=popover]").popover();
+		$("[data-toggle=popover]").popover('hide');
+		$(".courseCart").click(function() {
+			$("[data-toggle=popover]").popover();
+			listProfessors();
+		});
+		$('body').on('hidden.bs.popover', function (e) {
+			    $(e.target).data("bs.popover").inState = { click: false, hover: false, focus: false }
+		});
+		$('html').on('click', function(e) {
+			if (typeof $(e.target).data('original-title') == 'undefined' && !$(e.target).parents().is('.popover.in')) {
+				$('[data-original-title]').popover('hide');
+				$('[data-original-title]').popover();
+				$('[data-original-title]').popover('hide');
+			}
+		});
+	}
+	var addRemoveButton = function() {
+		$('.courseCart').remove();
+		$('p.title')[0].innerHTML = $('p.title')[0].innerHTML + "<p class='courseCart'><small id='remove'><i class='fa fa-trash-o'></i> Remove from My Cart</small></p>";
+		$('#remove').click(function(){
+			$("#remove").remove();
+			addCartButton();
+			localStorage.removeItem(title);
+		});
+	}
 	addToCourseCart = function(instructor) {
+		$('[data-original-title]').popover('hide');
 		if (typeof(Storage) !== "undefined") {
 			localStorage.setItem(title, JSON.stringify(findInstructorData(instructor)));
+			addRemoveButton();
 		} else {
 			alert("Sorry! Your browser does not support this feature. Please try again with a different browser.");
 		}
-		$('[data-original-title]').popover('hide');
 	}
-	$("p.title")[0].innerHTML = $("p.title")[0].innerHTML + "<p class='courseCart'> <small id='popup' data-html='true' data-toggle='popover' data-content='' title='Select Professor'><i class='fa fa-plus-square-o' aria-hidden='true'></i> Add to My Cart</small></p>";
-	var list = "<div id='divList'> <ul class='professorList'>";
-	var close = "$('[data-original-title]').popover('hide');";
-	for (var i = 0; i < instructors.length; i++) {
-		list += "<li><button onclick='addToCourseCart(instructors[" + i  +"]);'>" + instructors[i]  + "</button></li>";
+	if (typeof(Storage) !== "undefined" && localStorage.getItem(title)) {
+		addRemoveButton();
+	} else {
+		addCartButton();
 	}
-	list += "</ul></div>";
-	$("#popup").attr("data-content",list);
-	$("[data-toggle=popover]").popover();
-	$("[data-toggle=popover]").popover('hide');
-	$(".courseCart").click(function() {
-		$("[data-toggle=popover]").popover();
-	});
-	$('body').on('hidden.bs.popover', function (e) {
-		    $(e.target).data("bs.popover").inState = { click: false, hover: false, focus: false }
-	});
-	$('html').on('click', function(e) {
-		if (typeof $(e.target).data('original-title') == 'undefined' && !$(e.target).parents().is('.popover.in')) {
-			$('[data-original-title]').popover('hide');
-			$('[data-original-title]').popover();
-			$('[data-original-title]').popover('hide');
-		}
-	});
-
-}, 1000);
+}, 1500);
