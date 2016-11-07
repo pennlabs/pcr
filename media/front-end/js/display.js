fd881e4ea48f833fba0d8b281db294e890ef4ca7
Adam Domingoes <adamdom@wharton.upenn.edu>
2016-11-07 14:49:45 -0500

    More code style changes

--- media/front-end/js/display.js
+++ media/front-end/js/display.js
@@ -6,7 +6,8 @@ var update = function() {
       var course = JSON.parse(localStorage.getItem(key));
       if (course.info) {
         for (var i = 0; i < course.info.length; i++) {
-          if (course.info[i].recent >= 0 && !$("#" + course.course.split(" ").join("")).hasClass("courseInBoxGrayed")) {
+          if (course.info[i].recent >= 0 && !$("#" + 
+          course.course.split(" ").join("")).hasClass("courseInBoxGrayed")) {
             if (sum_recent[course.info[i].category]) {
               sum_recent[course.info[i].category] += course.info[i].recent;
               sum_average[course.info[i].category] += course.info[i].average;
@@ -14,27 +15,37 @@ var update = function() {
               sum_recent[course.info[i].category] = course.info[i].recent;
               sum_average[course.info[i].category] = course.info[i].average;
             }
-            count[course.info[i].category] = count[course.info[i].category] ? count[course.info[i].category] + 1 : 1;
+      const cat = count[course.info[i].category]
+            count[course.info[i].category] = cat ? cat + 1 : 1;
           }
         }
       }
     }
-    var listings=[["rCourseQuality","courseNum"],["rInstructorQuality","instructorNum"],["rDifficulty","difficultyNum"],["rWorkRequired","workloadNum"]];
+    var listings=[["rCourseQuality","courseNum"],
+                  ["rInstructorQuality","instructorNum"],
+      ["rDifficulty","difficultyNum"],
+      ["rWorkRequired","workloadNum"]];
     for (var i = 0; i < listings.length; i++) {
       if (count[listings[i][0]]) {
-        $("#"+listings[i][1])[0].innerHTML=(sum_average[listings[i][0]]/count[listings[i][0]]).toFixed(1); 
+        $("#"+listings[i][1])[0].innerHTML =
+          (sum_average[listings[i][0]] / count[listings[i][0]]).toFixed(1); 
       } else {
-        $("#"+listings[i][1])[0].innerHTML= "0.0";
+        $("#"+listings[i][1])[0].innerHTML = "0.0";
       }
     }
   }
 var display = function() {  
   var formCourse = function(name, v1, v2, v3, v4) {
     var nospaces = name.split(" ").join("");
-    var html = "<div onclick='$(\"#" + nospaces + "\").toggleClass(\"courseInBoxGrayed\"); update();'"
-      + "id='"+ nospaces +"' class='tooltip courseInBox'>"
-      + '<i onclick="$(\'#'+ nospaces + '\').remove();localStorage.removeItem(\'' + name + '\');update();" class="fa fa-times" aria-hidden="true"></i>'
-      + name + '<span class="tooltiptext">' + "Course Quality: " + v1 + "<br> Instructor Quality: " + v2 + "<br> Difficulty: " + v3 + "<br> Workload: "
+    var html = "<div onclick='$(\"#" + nospaces + 
+               "\").toggleClass(\"courseInBoxGrayed\"); update();'" +
+               "id='" + nospaces + "' class='tooltip courseInBox'>" +
+               '<i onclick="$(\'#'+ nospaces + 
+               '\').remove();localStorage.removeItem(\'' +
+               name + '\');update();" class="fa fa-times" aria-hidden="true"></i>' +
+               name + '<span class="tooltiptext">' + "Course Quality: " +
+               v1 + "<br> Instructor Quality: " + v2 + 
+               "<br> Difficulty: " + v3 + "<br> Workload: "
       + v4 + "</span>" + "</div>";
     return html;
   }
@@ -59,7 +70,8 @@ var display = function() {
       }
     }
     if (course.course)
-      $("#courseBox").html($("#courseBox").html() + formCourse(course.course, quality, instructor, difficulty, workload));
+      $("#courseBox").html($("#courseBox").html() + 
+        formCourse(course.course, quality, instructor, difficulty, workload));
   }  
   update();
   setInterval(update, 1000);
--- media/front-end/js/loader.js
+++ media/front-end/js/loader.js
@@ -1,7 +1,9 @@
 var listProfessors = function() {
   var list = "<div id='divList'><ul class='professorList'>";
   for (var i = 0; i < instructors.length; i++) {
-    list += "<li><button id='" + instructors[i].split(" ").join("") + "' onclick='addToCourseCart(instructors[" + i  +"]);'>" + instructors[i]  + "</button></li>";
+    list += "<li><button id='" + instructors[i].split(" ").join("") + 
+      "' onclick='addToCourseCart(instructors[" + i  +"]);'>" +
+      instructors[i]  + "</button></li>";
   }
   list += "</ul></div>";
   $("#popup").attr("data-content",list);
@@ -11,8 +13,13 @@ var listAlphabet = function(num) {
   var table = '<p>Filter by Last Name:</p><table class="professorList"><tr>';
   for (var i = 1; i <= 26; i++) {
     var character = String.fromCharCode(i+64)
-    table += '<td><button id="letter' + character + '">' + character + '</button></td>';
-    if (!instructors.reduce(function(a,b){return new RegExp("(\\s" + character + "[A-Z])").test(b) || a},false)) {
+    table += '<td><button id="letter' + character + '">' +
+              character + '</button></td>';
+    if (!instructors.reduce(
+          function(a,b){
+            return new RegExp("(\\s" + character 
+                + "[A-Z])").test(b) || a
+          }, false)) {
       $("#letter" + character).addClass('grayedOut'); 
     } else {
       var command = "listAlphabet('" + character + "');";
@@ -24,13 +31,23 @@ var listAlphabet = function(num) {
   table += '</tr></table><div id="filteredProfessors"></div>';
   $("#popup").attr("data-content", table);
   if (num) {
-    $("#filteredProfessors").html(instructors.reduce(function(a,b){return new RegExp("(\\s" + num + "[A-Z])").test(b) ?  a+"<li><button id='"+b.split(" ").join("")+"' onclick='addToCourseCart(\""+b+"\");'>"+b+"</button></li>" : a}, "<ul class='professorList'>") + "</ul>");
+    $("#filteredProfessors").html(instructors.reduce(
+          function(a,b){
+            return new RegExp("(\\s" + num + "[A-Z])").test(b) ?  a + 
+              "<li><button id='" + b.split(" ").join("") + 
+              "' onclick='addToCourseCart(\""+b+"\");'>"+ b +
+              "</button></li>" : a
+          }, "<ul class='professorList'>") + "</ul>");
     $("div.arrow").css("top", "103px");
   }
 }
 
 var addCartButton = function() {
-  $("#banner-score")[0].innerHTML = $("#banner-score")[0].innerHTML + "<span class='button courseCart'><small id='popup' data-html='true' data-toggle='popover' data-placement='left' data-content='' title='Select Professor'><i class='fa fa-cart-plus' aria-hidden='true'></i>  Add to My Cart</small></span>";
+  $("#banner-score")[0].innerHTML = $("#banner-score")[0].innerHTML +
+    "<span class='button courseCart'><small id='popup' data-html='true'" +
+    " data-toggle='popover' data-placement='left' data-content=''" + 
+    "title='Select Professor'><i class='fa fa-cart-plus' aria-hidden='true'></i>i" +
+    "  Add to My Cart</small></span>";
   if (instructors.length <= 15)
     listProfessors();
   else
@@ -49,7 +66,9 @@ var addCartButton = function() {
     $(e.target).data("bs.popover").inState = { click: false, hover: false, focus: false }
   });
   $('html').on('click', function(e) {
-    if (typeof $(e.target).data('original-title') == 'undefined' && !$(e.target).is('button')  && !$(e.target).parents().is('.popover.in')) {
+    if (typeof $(e.target).data('original-title') == 'undefined' &&
+        !$(e.target).is('button')  && 
+        !$(e.target).parents().is('.popover.in')) {
       $('[data-original-title]').popover('hide');
       $('[data-original-title]').popover();
       $('[data-original-title]').popover('hide');
@@ -58,7 +77,10 @@ var addCartButton = function() {
 }
 var addRemoveButton = function() {
   $('.courseCart').remove();
-  $('#banner-score')[0].innerHTML = $('#banner-score')[0].innerHTML + "<span class='button courseCart'><small id='remove'><i class='fa fa-trash-o'></i> Remove from My Cart</small></span>";
+  $('#banner-score')[0].innerHTML = $('#banner-score')[0].innerHTML +
+    "<span class='button courseCart'>" +
+    "<small id='remove'><i class='fa fa-trash-o'></i>" +
+    " Remove from My Cart</small></span>";
   $('#remove').click(function(){
     $("#remove").remove();
     addCartButton();
@@ -71,6 +93,7 @@ addToCourseCart = function(instructor) {
     localStorage.setItem(title, JSON.stringify(instructor_data[instructor]));
     addRemoveButton();
   } else {
-    alert("Sorry! Your browser does not support this feature. Please try again with a different browser.");
+    alert("Sorry! Your browser does not support this feature." +
+        " Please try again with a different browser.");
   }
 }
