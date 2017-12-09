01e1031a2aea4156aa93370812e29fad55ab48eb
Eric Wang <ezwang1@gmail.com>
2017-12-09 16:15:22 -0500

    use url reverse lookups

--- apps/pcr_detail/urls.py
+++ apps/pcr_detail/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^instructor/(.*)$', views.instructor),
-    url(r'^course/(\w+)-(\w+)$', views.course),
-    url(r'^department/(\w+)$', views.department),
+    url(r'^instructor/(.*)$', views.instructor, name="instructor"),
+    url(r'^course/(\w+-\w+)$', views.course, name="course"),
+    url(r'^department/(\w+)$', views.department, name="department"),
 ]
