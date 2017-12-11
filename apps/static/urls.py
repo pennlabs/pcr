e8b43a3698ed85dceaed78e497fead3cbe09c5c9
Eric Wang <ezwang1@gmail.com>
2017-12-11 10:51:06 -0500

    match exact

--- apps/static/urls.py
+++ apps/static/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    url(r'logout', views.logout, name="logout"),
-    url(r'about', views.about, name="about"),
-    url(r'cart', views.cart, name="cart"),
-    url(r'faq', views.faq, name="faq"),
+    url(r'^logout$', views.logout, name="logout"),
+    url(r'^about$', views.about, name="about"),
+    url(r'^cart$', views.cart, name="cart"),
+    url(r'^faq$', views.faq, name="faq"),
     url(r'^chrome/api/(?P<path>.*)$', views.proxy)
 ]
