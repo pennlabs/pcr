6b30fd121e139b8eec20ecd786748883dfda931e
Eric Wang <ezwang1@gmail.com>
2018-11-30 19:03:35 -0500

    autogenerate token for pcr

--- lib/api/api.py
+++ lib/api/api.py
@@ -5,10 +5,18 @@ import json
 from django.conf import settings
 
 from .memoize import memoize
+from api.apiconsumer.models import APIConsumer
 
 
 @memoize
 def api(domain, *args, **kwargs):
+    if not "token" in kwargs:
+        kwargs["token"], _ = APIConsumer.objects.get_or_create(permission_level=9001, defaults={
+            "name": "Penn Course Review",
+            "email": "pennappslabs@gmail.com",
+            "description": "The Penn Course Review app.\nHas access to super-secret stuff."
+        })
+        kwargs["token"] = kwargs["token"].token
     assert domain.endswith("/")
     path = "".join(
         (domain, "/".join([str(arg) for arg in args])))
