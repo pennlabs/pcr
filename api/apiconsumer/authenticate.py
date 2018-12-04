eeb40a2831684bdf55213ef382e13e205bbad2b6
Eric Wang <ezwang1@gmail.com>
2018-12-04 13:01:27 -0500

    json response for errors

--- api/apiconsumer/authenticate.py
+++ api/apiconsumer/authenticate.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import JsonResponse
 from .models import APIConsumer, generate_api_consumer
 import requests
 
@@ -26,7 +26,7 @@ class Authenticate(object):
         try:
             token = request.GET['token']
         except:
-            return HttpResponse("No token provided.", status=403)
+            return JsonResponse({"error": "No token provided."}, status=403)
 
         try:
             consumer = APIConsumer.objects.get(token=token)
@@ -44,4 +44,4 @@ class Authenticate(object):
             request.consumer = consumer
             return None  # continue rendering
         else:
-            return HttpResponse("Invalid token.", status=403)
+            return JsonResponse({"error": "Invalid token."}, status=403)
