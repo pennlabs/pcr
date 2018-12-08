b5e95d421cb3305f623427ff1eb9928c0eb36e49
Eric Wang <ezwang1@gmail.com>
2018-12-07 19:07:08 -0500

    add instructor url patch

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import include, url
 
+from .courses.views import instructor_main
+
 urlpatterns = [
     url(r'^', include('api.courses.urls', namespace='api')),
+    url(r'^instructor/(.*)$', instructor_main, name="instructor"),
 ]
