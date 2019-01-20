10b2fd29f8a9a6a9ae3e6b64551f1d811b8c9e9f
Eric Wang <ezwang1@gmail.com>
2019-01-20 15:18:31 -0500

    make api v1 endpoint still work

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import include, url
 from .courses.views import instructor_main
 
 urlpatterns = [
-    url(r'^', include('api.courses.urls', namespace='api')),
+    url(r'^(v1/)?', include('api.courses.urls', namespace='api')),
     url(r'^instructor/(.*)$', instructor_main, name="instructor"),
 ]
