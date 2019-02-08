3b02d7e843cee5cf1b89f81b7e717481f39872c0
Eric Wang <ezwang1@gmail.com>
2019-02-08 18:09:29 -0500

    super efficient code to retrieve ratings table

--- api/courses/display.py
+++ api/courses/display.py
@@ -3,7 +3,7 @@ import re
 from django.http import JsonResponse
 from django.db.models import Avg
 
-from .models import Alias, Course, Section, Review, ReviewBit
+from .models import Alias, Course, Section, Review, ReviewBit, Instructor
 
 
 def display_course(request, course):
@@ -17,10 +17,21 @@ def display_course(request, course):
     reviewbits_average = ReviewBit.objects.filter(review__in=reviews).values("field").annotate(score=Avg('score'))
     reviewbits_recent = ReviewBit.objects.filter(review__in=reviews, review__section__course__semester=semester).values("field").annotate(score=Avg('score'))
 
+    instructors = {inst["id"]: {"name": (inst["first_name"] + " " + inst["last_name"]).strip(), "average_reviews": {}, "recent_reviews": {}} for inst in Instructor.objects.filter(section__in=sections).values("id", "first_name", "last_name")}
+    instructor_average_ratings = ReviewBit.objects.filter(review__in=reviews).values("field", "review__section__instructors").annotate(score=Avg('score'))
+    instructor_recent_ratings = ReviewBit.objects.filter(review__in=reviews).order_by("review__section__course__semester").values("field", "review__section__instructors", "score")
+
+    for rating in instructor_average_ratings:
+        instructors[rating["review__section__instructors"]]["average_reviews"][rating["field"]] = round(rating["score"], 2)
+
+    for rating in instructor_recent_ratings:
+        instructors[rating["review__section__instructors"]]["recent_reviews"][rating["field"]] = round(rating["score"], 2)
+
     return JsonResponse({
         "code": "{} {}".format(dept, num),
         "name": course.name,
         "description": course.description.strip(),
         "average_ratings": {bit["field"]: round(bit["score"], 1) for bit in reviewbits_average},
-        "recent_ratings": {bit["field"]: round(bit["score"], 1) for bit in reviewbits_recent}
+        "recent_ratings": {bit["field"]: round(bit["score"], 1) for bit in reviewbits_recent},
+        "instructors": instructors,
     })
