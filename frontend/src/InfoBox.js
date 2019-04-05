547c47aebdeb6c240b528ee97a699f591d7d0b5f
Eric Wang <ezwang1@gmail.com>
2019-04-05 17:42:37 -0400

    refactor data retrieval and implement tags

--- frontend/src/InfoBox.js
+++ frontend/src/InfoBox.js
@@ -6,64 +6,35 @@ import ScoreboxRow from './ScoreboxRow'
 class InfoBox extends Component {
 
   constructor(props) {
-
-    super(props)
+    super(props);
 
     this.state = {
-      items: null,
-      average_ratings: null,
+      items: this.props.data,
+      average_ratings: this.props.data.average_ratings,
+      recent_ratings: this.props.data.recent_ratings,
       tag_data: null
-    }
+    };
   }
 
   componentDidMount() {
-    // "http://localhost:8000/api/display/instructor/6470-RAJIV-GANDHI?token=public"
-    // "http://localhost:8000/api/display/course/CIS-121?token=public"
-    // "http://localhost:8000/api/display/department/CIS?token=public"
-    fetch("http://localhost:8000/api/display/department/CIS?token=public")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log('result')
-          console.log(result)
-          this.setState(state => ({
-            items: result,
-            average_ratings: result.average_ratings,
-            recent_ratings: result.recent_ratings
-          }))
-        })
-
-    fetch("http://localhost:8000/live/CIS-121")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState(state => ({
-            tag_data: result
-          }))
-        })
+    if (this.props.type === "course") {
+       fetch("http://localhost:8000/live/" + encodeURIComponent(this.props.code))
+         .then(res => res.json())
+         .then(
+           (result) => {
+             this.setState(state => ({
+               tag_data: result
+             }))
+           });
+    }
   }
 
   render() {
-    // console.log("hi")
-    // console.log(window)
-    var pageType = window.pageType // ["COURSES", "INSTRUCTOR", "DEPARTMENT"]
-    console.log(window.pageType)
-    // console.log("pagetype")
-    // console.log(pageType)
-    var pageIdentifier = window.pageIdentifier
-    console.log(pageIdentifier)
-    // console.log(pageIdentifier)
-    // console.log(window)
+    const pageType = this.props.type;
 
     if (!this.state.items) {
-      return (<h1>Loading data</h1>);
+        return <h1>Loading data...</h1>;
     }
-    if (!this.state.tag_data) {
-      return (<h1>Null tag data</h1>);
-    }
-
-    // do not show pages if course is not offered this semester or if page type is instructor
-    var noTags = !this.state.tag_data.term // noTags is true if term is Null
 
     return (
         <div className="box">
@@ -73,8 +44,8 @@ class InfoBox extends Component {
                 <div className="title">{this.state.items.code}
 
                   <span className="float-right">
-                    <span id="popup" className="courseCart btn btn-action" title="Add to Cart"><i className="fa fa-fw fa-cart-plus"></i></span>
-                    <a target="_blank" title="Get Alerted" href="https://penncoursealert.com/?course=CIS-121&amp;source=pcr" className="btn btn-action"><i className="fas fa-fw fa-bell"></i></a>
+                    <span id="popup" className="courseCart btn btn-action" title="Add to Cart"><i className="fa fa-fw fa-cart-plus"></i></span>{' '}
+                    <a target="_blank" rel="noopener noreferrer" title="Get Alerted" href={"https://penncoursealert.com/?course=" + this.props.code + "&source=pcr"} className="btn btn-action"><i className="fas fa-fw fa-bell"></i></a>
                   </span>
 
                 </div>
@@ -83,11 +54,8 @@ class InfoBox extends Component {
 
                 <p className="subtitle">{this.state.items.name}</p>
 
-                { !noTags &&
-                  <Tags
-                    term={this.state.tag_data.term}
-                    credits={this.state.tag_data.credits}
-                    />
+                { (this.state.tag_data && this.state.tag_data.term) &&
+                  <Tags {...this.state.tag_data} />
                 }
               </div>
             }
@@ -108,7 +76,7 @@ class InfoBox extends Component {
 
           </div>
 
-          { pageType != "department" &&
+          { pageType !== "department" &&
             <div id="banner-score">
               <ScoreboxRow
                 value="Average"
@@ -130,7 +98,7 @@ class InfoBox extends Component {
             <div className="department-content">
               <div id="row-select-placeholder">
                   <object type="image/svg+xml" data="/static/image/selectrow.svg">
-                    <img src="/static/image/selectrow.svg" />
+                    <img alt="Select Row" src="/static/image/selectrow.svg" />
                   </object>
                   <div id="row-select-text">Select a few rows to begin comparing courses.</div>
               </div>
