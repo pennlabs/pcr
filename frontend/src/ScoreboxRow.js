be2bdd33763481b1e92d1850ab8beeaa349ddf8d
Eric Wang <ezwang1@gmail.com>
2019-04-23 17:41:33 -0400

    fix rating colors

--- frontend/src/ScoreboxRow.js
+++ frontend/src/ScoreboxRow.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 
 class ScoreboxRow extends Component {
-  // state
-  constructor(props) {
-
-    super(props)
-
-    this.state = {
-    }
-  }
-
   render() {
     function numOrNA(num) {
         return isNaN(num) ? "N/A" : num.toFixed(1);
+    }
+
+    function getColor(num) {
+        if (isNaN(num)) {
+            return "rating-good";
+        }
+
+        num = num.toFixed(1);
+
+        if (num < 2) {
+            return "rating-bad";
+        }
+
+        if (num < 3) {
+            return "rating-okay";
+        }
+
+        return "rating-good";
     }
 
     return (
@@ -22,15 +31,15 @@ class ScoreboxRow extends Component {
               <p className="subtitle">{this.props.num_sections} Sections</p>
           </div>
           <div className="scoreboxrow">
-              <div className="scorebox course rating-okay">
+              <div className={"scorebox course " + getColor(this.props.course)}>
                   <p className="num">{numOrNA(this.props.course)}</p>
                   <p className="desc">Course</p>
               </div>
-              <div className="scorebox instructor rating-okay">
+              <div className={"scorebox instructor " + getColor(this.props.instructor)}>
                   <p className="num">{numOrNA(this.props.instructor)}</p>
                   <p className="desc">Instructor</p>
               </div>
-              <div className="scorebox difficulty rating-good">
+              <div className={"scorebox difficulty " + getColor(this.props.difficulty)}>
                   <p className="num">{numOrNA(this.props.difficulty)}</p>
                   <p className="desc">Difficulty</p>
               </div>
