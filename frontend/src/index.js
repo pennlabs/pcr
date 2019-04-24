ed9a7940dc4c41ccf5545f9d9f04c4a609757a91
Eric Wang <ezwang1@gmail.com>
2019-04-24 16:21:41 -0400

    Revert "add google analytics"
    This reverts commit f818f59d188dff1fb2ec6d912a3c67149ed869e2.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Switch, Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import ReviewPage from './ReviewPage';
 import InfoPage from './InfoPage';
 import AuthPage from './AuthPage';
 
-var history = createBrowserHistory();
-history.listen(function(loc) {
-    window.ga('set', 'page', loc.pathname + loc.search);
-    window.ga('send', 'pageview');
-});
-
-ReactDOM.render(<Router history={history}>
+ReactDOM.render(<Router>
     <Switch>
         <Route exact path="/" component={ReviewPage} />
         <Route path="/:type(course|department|instructor)/:code" component={AuthPage} />
