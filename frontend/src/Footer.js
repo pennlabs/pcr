128ba091e18cc3238267b02c8a683af8c7501bf0
Eric Wang <ezwang1@gmail.com>
2019-04-23 23:25:45 -0400

    change copyright to be more friendly

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -8,9 +8,10 @@ class Footer extends Component {
             <div id="footer-inner">
                 <Link to="/about">About</Link> | <Link to="/faq">FAQs</Link> | <a href="https://docs.google.com/spreadsheet/viewform?formkey=dFNZYW92cFM1YnpKUzlLcXRDZVQ4VGc6MQ#gid=0faq">Feedback</a> | <a href="/logout">Logout</a>
 
-                <p id="copyright">
-                Copyright &copy; {new Date().getFullYear()} <a href="https://pennlabs.org"><strong>Penn Labs</strong></a> | Hosted by <a href="https://stwing.upenn.edu/"><strong>STWing</strong></a>
-                </p>
+                    <p id="copyright">
+                    Made with <i style={{color: '#F56F71'}} className="fa fa-heart" /> by <a href="https://pennlabs.org"><strong>Penn Labs</strong></a> | Hosted by <a href="https://stwing.upenn.edu/"><strong>STWing</strong></a>
+                    </p>
+                </div>
             </div>
         </div>);
     }
