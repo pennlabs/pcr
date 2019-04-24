f818f59d188dff1fb2ec6d912a3c67149ed869e2
Eric Wang <ezwang1@gmail.com>
2019-04-24 14:50:56 -0400

    add google analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch, Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import ReviewPage from './ReviewPage';
 import InfoPage from './InfoPage';
 import AuthPage from './AuthPage';
 
-ReactDOM.render(<Router>
+var history = createBrowserHistory();
+history.listen(function(loc) {
+    window.ga('set', 'page', loc.pathname + loc.search);
+    window.ga('send', 'pageview');
+});
+
+ReactDOM.render(<Router history={history}>
     <Switch>
         <Route exact path="/" component={ReviewPage} />
         <Route path="/:type(course|department|instructor)/:code" component={AuthPage} />
