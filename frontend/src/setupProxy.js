57aaebd7bd4cbe68e7eac1e33ff105c0e8c05455
Eric Wang <ezwang1@gmail.com>
2019-05-09 20:00:40 -0400

    make dev easier

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,9 @@
+const proxy = require('http-proxy-middleware');
+
+
+module.exports = function(app) {
+    app.get('/api/display/token', function(req, res) {
+        res.redirect(req.query.redirect);
+    });
+    app.use(proxy('/api', { target: 'https://penncoursereview.com/', secure: false }));
+};
