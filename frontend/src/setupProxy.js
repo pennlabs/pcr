f766dff47417ce1818c2981066c14fe0b82010ec
Eric Wang <ezwang1@gmail.com>
2019-10-12 09:36:07 -0400

    more control over proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -5,8 +5,10 @@ module.exports = function(app) {
     app.get('/api/display/token', function(req, res) {
         res.redirect(req.query.redirect);
     });
+    const proxy_url = process.env.PROXY_URL || 'https://penncoursereview.com';
     app.use(proxy('/api', {
-        target: 'https://penncoursereview.com',
+        logLevel: 'debug',
+        target: proxy_url,
         changeOrigin: true,
         secure: false
     }));
