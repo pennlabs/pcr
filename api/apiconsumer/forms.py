baac8d546e01d8781f6ab7a0e0382fe37fcc6c99
Daniel Tao <danieltaox@gmail.com>
2019-10-18 01:34:28 -0400

    fix spacing in forms.py and tokens.py

--- api/apiconsumer/forms.py
+++ api/apiconsumer/forms.py
@@ -1,13 +1,16 @@
 from django import forms
 from .models import APIConsumer
 
+
 class TokenForm(forms.ModelForm):
     name = forms.CharField(max_length=200, help_text='Required')
     email = forms.EmailField(max_length=75, help_text='Required')
     description = forms.CharField()
+
     class Meta:
         model = APIConsumer
         fields = ('name', 'email', 'description')
+
 
 class ResetTokenForm(forms.Form):
     email = forms.EmailField(max_length=75, help_text='Required')
