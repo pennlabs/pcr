d05092c90568faf27517a14fa432fbef74819766
Daniel Tao <danieltaox@gmail.com>
2019-10-18 01:35:34 -0400

    remove unused vars and fix spacing in views.py

--- api/apiconsumer/views.py
+++ api/apiconsumer/views.py
@@ -1,21 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponse
-from django.conf import settings
-
-from django.contrib.auth import login, authenticate
 from .forms import TokenForm, ResetTokenForm
 
-from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from .tokens import account_activation_token
 
 from .models import APIConsumer
-from django.core.mail import EmailMessage
 from django.core.mail import send_mail
+from django.core.exceptions import ObjectDoesNotExist
 
-import requests
 
 def form(request):
     if request.method == 'POST':
@@ -26,21 +21,20 @@ def form(request):
             user = form.save(commit=False)
             user.is_active = False
             user.save()
-            current_site = get_current_site(request)
             mail_subject = 'Activate Your API Consumer Account.'
             message = render_to_string('api/activate_api_consumer.html', {
                 'user': user,
                 'domain': 'http://localhost:8000/',
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode('utf-8'),
-                'token':account_activation_token.make_token(user),
+                'token': account_activation_token.make_token(user),
             })
-            # print(urlsafe_base64_encode(force_bytes(user.pk)).decode('utf-8'), account_activation_token.make_token(user))
             to_email = form.cleaned_data.get('email')
             send_mail(mail_subject, message, 'pennappslabs@gmail.com', [to_email], fail_silently=False)
             return HttpResponse('Please confirm your email address to complete the registration')
     else:
         form = TokenForm()
     return render(request, 'api/form.html', {'form': form})
+
 
 def activate(request, uidb64, token):
     # set to expire (if user doesn't activate) and can't access again (already done)
@@ -52,9 +46,6 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        # login(request, user)
-        # return redirect('home')
-        current_site = get_current_site(request)
         mail_subject = 'Your API Consumer Account Token'
         message = render_to_string('api/api_consumer_success.html', {
             'user': user,
@@ -62,11 +53,11 @@ def activate(request, uidb64, token):
             'token': user.token,
         })
         to_email = user.email
-        # print(to_email, user.token)
         send_mail(mail_subject, message, 'pennappslabs@gmail.com', [to_email, 'pennappslabs@gmail.com'], fail_silently=False)
         return HttpResponse('Thank you for your email confirmation! Check your email for you API token.')
     else:
         return HttpResponse('Activation link is invalid!')
+
 
 def reset_token(request):
     if request.method == 'POST':
@@ -75,15 +66,13 @@ def reset_token(request):
             to_email = form.cleaned_data.get('email')
             try:
                 user = APIConsumer.objects.get(email=to_email)
-            except:
+            except ObjectDoesNotExist:
                 return HttpResponse('Invalid email: APIConsumer user does not exist')
-            # print(user)
-            current_site = get_current_site(request)
             mail_subject = 'Your API Consumer Account Token (Resent)'
             message = render_to_string('api/api_consumer_success.html', {
                 'user': user,
                 'domain': 'http://localhost:8000/',
-                'token':user.token,
+                'token': user.token,
             })
             send_mail(mail_subject, message, 'pennappslabs@gmail.com', [to_email], fail_silently=False)
             return HttpResponse('Your API token was resent.')
