ba6f63e2a599c5dfde00414264571e8eab3f0d69
Eric Wang <ezwang1@gmail.com>
2019-12-20 13:37:36 -0500

    only make token if not exists

--- api/apiconsumer/management/commands/maketoken.py
+++ api/apiconsumer/management/commands/maketoken.py
@@ -7,10 +7,9 @@ class Command(BaseCommand):
     help = 'Create a token for development'
 
     def handle(self, **options):
-        token = APIConsumer(name='public',
-                            email='',
-                            description='',
-                            token='public',
-                            permission_level=2,
-                            )
-        token.save()
+        APIConsumer.objects.get_or_create(token='public', defaults={
+            'name': 'Public',
+            'email': '',
+            'description': '',
+            'permission_level': 2
+        })
