d412136bcab62196203de4776fed0394b22850f2
Armaan Tobaccowalla <13340433+ArmaanT@users.noreply.github.com>
2019-12-21 13:27:25 -0500

    Add Static Root to django

--- settings.py
+++ settings.py
@@ -93,6 +93,7 @@ MEDIA_URL = ''
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Path to local staticfiles
 STATICFILES_DIRS = [
