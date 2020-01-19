06f869c5931acc84d440dab2d9015902a031511e
Daniel Tao <dtao@seas.upenn.edu>
2020-01-19 13:53:36 -0500

    fix Footer warnings

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -10,10 +10,10 @@ class Footer extends Component {
     render() {
         return (<div style={this.props.style} id="footer">
             <div id="footer-inner">
-                <Link to="/about">About</Link> | <Link to="/faq">FAQs</Link> | <a target="_blank" rel="noopener noreferrer" href="https://airtable.com/shrVygSaHDL6BswfT">Feedback</a> | <a onClick={redirect_for_logout} href="#">Logout</a>
+                <Link to="/about">About</Link> | <Link to="/faq">FAQs</Link> | <a target="_blank" rel="noopener noreferrer" href="https://airtable.com/shrVygSaHDL6BswfT">Feedback</a> | <a onClick={redirect_for_logout} href="/">Logout</a>
 
                 <p id="copyright">
-                Made with <i style={{color: '#F56F71'}} className="fa fa-heart" /> by <a href="https://pennlabs.org"><strong>Penn Labs</strong></a> | Hosted by <a href="https://www.stwing.upenn.edu/"><strong>STWing</strong></a>
+                Made with <i style={{color: '#F56F71'}} className="fa fa-heart" /> by <a href="https://pennlabs.org"><strong>Penn Labs</strong></a> | Hosted by <a href="https://stwing.upenn.edu/"><strong>STWing</strong></a>
                 </p>
             </div>
         </div>);
