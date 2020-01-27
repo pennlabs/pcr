ccd5be59e3f85233598d77cd971deb154d822f67
Daniel Tao <dtao@seas.upenn.edu>
2020-01-27 17:28:09 -0500

    destructure ErrorBox props

--- frontend/src/ErrorBox.js
+++ frontend/src/ErrorBox.js
@@ -3,12 +3,12 @@ import React from 'react'
 /**
  * A component that appears if an error occurs in the page.
  */
-export default () => (
+export default ({ children, detail }) => (
   <div style={{ textAlign: 'center', padding: 45 }}>
     <i className='fa fa-exclamation-circle' style={{ fontSize: '150px', color: '#aaa' }} />
-    <h1 style={{ fontSize: '1.5em', marginTop: 15 }}>{this.props.children}</h1>
+    <h1 style={{ fontSize: '1.5em', marginTop: 15 }}>{children}</h1>
     <small>
-      {this.props.detail}
+      {detail}
       {' '}
       If this problem persists, contact Penn Labs at
       {' '}
