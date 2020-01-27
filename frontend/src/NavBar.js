ce993250d084bfa6eccb78e0f3df38567f76a12d
Daniel Tao <dtao@seas.upenn.edu>
2020-01-27 17:36:21 -0500

    lint NavBar

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -1,54 +1,53 @@
-import React, { Component } from 'react';
-import SearchBar from './SearchBar';
-import { Link } from 'react-router-dom';
-
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import SearchBar from './SearchBar'
 
 /**
  * The navigation bar at the top of the page, containing the logo, search bar, and cart icon.
  */
 class NavBar extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            courseCount: Object.keys(localStorage).filter((a) => !a.startsWith("meta-")).length
-        };
-
-        this.onStorageChange = this.onStorageChange.bind(this);
+    this.state = {
+      courseCount: Object.keys(localStorage).filter((a) => !a.startsWith('meta-')).length,
     }
 
-    onStorageChange() {
-        this.setState({
-            courseCount: Object.keys(localStorage).filter((a) => !a.startsWith("meta-")).length
-        });
-    }
+    this.onStorageChange = this.onStorageChange.bind(this)
+  }
 
-    componentDidMount() {
-        window.addEventListener("storage", this.onStorageChange);
-        window.onCartUpdated = this.onStorageChange;
-    }
+  onStorageChange() {
+    this.setState({
+      courseCount: Object.keys(localStorage).filter((a) => !a.startsWith('meta-')).length,
+    })
+  }
 
-    componentWillUnmount() {
-        window.removeEventListener("storage", this.onStorageChange);
-        window.onCartUpdated = null;
-    }
+  componentDidMount() {
+    window.addEventListener('storage', this.onStorageChange)
+    window.onCartUpdated = this.onStorageChange
+  }
 
-    render() {
-        return (
-            <div id="header">
-                <span className="float-left">
-                    <Link to="/" title="Go to Penn Course Review Home"><div id="logo"></div></Link>
-                    <SearchBar />
-                </span>
-                    <span className="float-right">
-                    <Link to="/cart" id="cart-icon" title="Course Cart">
-                        <i id="cart" className="fa fa-shopping-cart"></i>
-                        {this.state.courseCount > 0 && <span id="cart-count">{this.state.courseCount}</span>}
-                    </Link>
-                </span>
-            </div>
-        );
-    }
+  componentWillUnmount() {
+    window.removeEventListener('storage', this.onStorageChange)
+    window.onCartUpdated = null
+  }
+
+  render() {
+    return (
+      <div id='header'>
+        <span className='float-left'>
+          <Link to='/' title='Go to Penn Course Review Home'><div id='logo' /></Link>
+          <SearchBar />
+        </span>
+        <span className='float-right'>
+          <Link to='/cart' id='cart-icon' title='Course Cart'>
+            <i id='cart' className='fa fa-shopping-cart' />
+            {this.state.courseCount > 0 && <span id='cart-count'>{this.state.courseCount}</span>}
+          </Link>
+        </span>
+      </div>
+    )
+  }
 }
 
-export default NavBar;
+export default NavBar
