4bb8daa2b8b926b22a8ed2ed784e787b5a56863e
Daniel Tao <dtao@seas.upenn.edu>
2020-01-27 17:29:28 -0500

    convert Footer to functional component

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,24 +1,34 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { get_logout_url } from './api';
-
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { get_logout_url } from './api'
 
 /**
  * The footer of every page.
  */
-class Footer extends Component {
-    render() {
-        return (<div style={this.props.style} id="footer">
-            <div id="footer-inner">
-                <Link to="/about">About</Link> | <Link to="/faq">FAQs</Link> | <a target="_blank" rel="noopener noreferrer" href="https://airtable.com/shrVygSaHDL6BswfT">Feedback</a> | <a href={get_logout_url()}>Logout</a>
+const Footer = ({ style }) => (
+  <div style={style} id='footer'>
+    <div id='footer-inner'>
+      <Link to='/about'>About</Link>
+      {' '}|{' '}
+      <Link to='/faq'>FAQs</Link>
+      {' '}|{' '}
+      <a target='_blank' rel='noopener noreferrer' href='https://airtable.com/shrVygSaHDL6BswfT'>Feedback</a>
+      {' '}|{' '}
+      <a href={get_logout_url()}>Logout</a>
 
-                <p id="copyright">
-                Made with <i style={{color: '#F56F71'}} className="fa fa-heart" /> by <a href="https://pennlabs.org"><strong>Penn Labs</strong></a> | Hosted by <a href="https://stwing.upenn.edu/"><strong>STWing</strong></a>
-                </p>
-            </div>
-        </div>);
-    }
-}
+      <p id='copyright'>
+        Made with
+        {' '}
+        <i style={{ color: '#F56F71' }} className='fa fa-heart' />
+        {' '}by{' '}
+        <a href='https://pennlabs.org'><strong>Penn Labs</strong></a>
+        {' '}|{' '}
+        Hosted by
+        {' '}
+        <a href='https://stwing.upenn.edu/'><strong>STWing</strong></a>
+      </p>
+    </div>
+  </div>
+)
 
-
-export default Footer;
+export default Footer
