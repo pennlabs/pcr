1c4546db252dbb73b12f01e0a6dd21ff2e6f3181
Daniel Tao <dtao@seas.upenn.edu>
2020-01-27 17:43:00 -0500

    lint proxy config

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,18 +1,17 @@
-const proxy = require('http-proxy-middleware');
-
+const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
-    const proxy_url = process.env.PROXY_URL || 'http://localhost:8000';
-    app.use(proxy('/api', {
-        logLevel: 'debug',
-        target: proxy_url,
-        changeOrigin: true,
-        secure: false
-    }));
-    app.use(proxy('/accounts', {
-        logLevel: 'debug',
-        target: proxy_url,
-        changeOrigin: true,
-        secure: false
-    }));
-};
+  const proxy_url = process.env.PROXY_URL || 'http://localhost:8000'
+  app.use(proxy('/api', {
+    logLevel: 'debug',
+    target: proxy_url,
+    changeOrigin: true,
+    secure: false,
+  }))
+  app.use(proxy('/accounts', {
+    logLevel: 'debug',
+    target: proxy_url,
+    changeOrigin: true,
+    secure: false,
+  }))
+}
