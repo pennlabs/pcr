8e99db03f4a8cc9d6c8b5d2adf45ce1a02ad0d13
Daniel Tao <dtao@seas.upenn.edu>
2020-01-27 17:23:55 -0500

    convert ErrorBox to functional component

--- frontend/src/ErrorBox.js
+++ frontend/src/ErrorBox.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
-
+import React from 'react'
 
 /**
  * A component that appears if an error occurs in the page.
  */
-export default class ErrorBox extends Component {
-    render() {
-        return <div style={{ textAlign: 'center', padding: 45 }}>
-            <i className='fa fa-exclamation-circle' style={{ fontSize: '150px', color: '#aaa' }}></i>
-            <h1 style={{ fontSize: '1.5em', marginTop: 15 }}>{this.props.children}</h1>
-            <small>{this.props.detail} If this problem persists, contact Penn Labs at <a href='mailto:contact@pennlabs.org'>contact@pennlabs.org</a>.</small>
-        </div>
-    }
-}
+export default () => (
+  <div style={{ textAlign: 'center', padding: 45 }}>
+    <i className='fa fa-exclamation-circle' style={{ fontSize: '150px', color: '#aaa' }} />
+    <h1 style={{ fontSize: '1.5em', marginTop: 15 }}>{this.props.children}</h1>
+    <small>
+      {this.props.detail}
+      {' '}
+      If this problem persists, contact Penn Labs at
+      {' '}
+      <a href='mailto:contact@pennlabs.org'>contact@pennlabs.org</a>.
+    </small>
+  </div>
+)
