0c1c0d0f6b05ab1031f83fefa1d9edbfcf9d8e7c
Daniel Tao <dtao@seas.upenn.edu>
2020-01-27 17:30:56 -0500

    lint GoogleAnalytics component

--- frontend/src/GoogleAnalytics.js
+++ frontend/src/GoogleAnalytics.js
@@ -1,26 +1,25 @@
-import { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-
+import { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 
 /**
  * Dummy component to make Google Analytics work well with React router.
  */
+// TODO: Use useeffect to convert to functional component
 class GoogleAnalytics extends Component {
-    componentWillUpdate({ location, history }) {
-        if (location.pathname === this.props.location.pathname) {
-            return;
-        }
-
-        if (history.action === 'PUSH') {
-            window.ga('set', 'page', location.pathname + location.search);
-            window.ga('send', 'pageview');
-        }
+  componentWillUpdate({ location, history }) {
+    if (location.pathname === this.props.location.pathname) {
+      return
     }
 
-    render() {
-        return null;
+    if (history.action === 'PUSH') {
+      window.ga('set', 'page', location.pathname + location.search)
+      window.ga('send', 'pageview')
     }
+  }
+
+  render() {
+    return null
+  }
 }
 
-
-export default withRouter(GoogleAnalytics);
+export default withRouter(GoogleAnalytics)
