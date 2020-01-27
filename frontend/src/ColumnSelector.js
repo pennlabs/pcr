00d55f4848cc14845ff823bda82b0bab6b22ad34
Daniel Tao <dtao@seas.upenn.edu>
2020-01-27 17:20:48 -0500

    lint ColumnSelector component

--- frontend/src/ColumnSelector.js
+++ frontend/src/ColumnSelector.js
@@ -1,80 +1,79 @@
-import React, { Component } from 'react';
-import Popover from './Popover';
-
+import React, { Component } from 'react'
+import Popover from './Popover'
 
 /**
  * Used to select the columns that appear in a table.
  */
 class ColumnSelector extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props)
 
-        this.handleToggle = this.handleToggle.bind(this);
-        this.setAllColumns = this.setAllColumns.bind(this);
-        this.changeColumns = this.changeColumns.bind(this);
+    this.handleToggle = this.handleToggle.bind(this)
+    this.setAllColumns = this.setAllColumns.bind(this)
+    this.changeColumns = this.changeColumns.bind(this)
 
-        var defaultColumns = localStorage.getItem("meta-" + this.props.name);
-        if (defaultColumns) {
-            defaultColumns = JSON.parse(defaultColumns);
-        }
-        else {
-            defaultColumns = {
-                ...(props.type === "instructor" && {
-                    latest_semester: true,
-                    num_semesters: true,
-                }),
-                rInstructorQuality: true,
-                rCourseQuality: true,
-                rDifficulty: true,
-                rAmountLearned: true
-            };
-        }
-        this.defaultColumns = defaultColumns;
-        this.changeColumns(this.props.columns.map((a) => ({...a, show: a.required || !!defaultColumns[a.id]})));
+    let defaultColumns = localStorage.getItem(`meta-${this.props.name}`)
+    if (defaultColumns) {
+      defaultColumns = JSON.parse(defaultColumns)
+    } else {
+      defaultColumns = {
+        ...(props.type === 'instructor' && {
+          latest_semester: true,
+          num_semesters: true,
+        }),
+        rInstructorQuality: true,
+        rCourseQuality: true,
+        rDifficulty: true,
+        rAmountLearned: true,
+      }
     }
+    this.defaultColumns = defaultColumns
+    this.changeColumns(this.props.columns.map((a) => ({ ...a, show: a.required || !!defaultColumns[a.id] })))
+  }
 
-    changeColumns(cols) {
-        var newColumns = cols.reduce((map, obj) => {
-            map[obj.id] = obj.show;
-            return map;
-        }, {});
-        this.defaultColumns = Object.assign(this.defaultColumns, newColumns);
-        localStorage.setItem("meta-" + this.props.name, JSON.stringify(this.defaultColumns));
-        this.props.onSelect(cols);
+  changeColumns(cols) {
+    const newColumns = cols.reduce((map, obj) => {
+      map[obj.id] = obj.show
+      return map
+    }, {})
+    this.defaultColumns = Object.assign(this.defaultColumns, newColumns)
+    localStorage.setItem(`meta-${this.props.name}`, JSON.stringify(this.defaultColumns))
+    this.props.onSelect(cols)
+  }
+
+  handleToggle(i) {
+    return () => {
+      const columnsCopy = Array.from(this.props.columns)
+      columnsCopy[i] = { ...columnsCopy[i], show: !columnsCopy[i].show }
+      this.changeColumns(columnsCopy)
     }
+  }
 
-    handleToggle(i) {
-        return () => {
-            let columnsCopy = Array.from(this.props.columns);
-            columnsCopy[i] = {...columnsCopy[i], show: !columnsCopy[i].show};
-            this.changeColumns(columnsCopy);
-        };
+  setAllColumns(val) {
+    return () => {
+      const columnsCopy = this.props.columns.map((a) => ({ ...a, show: a.required || val }))
+      this.changeColumns(columnsCopy)
     }
+  }
 
-    setAllColumns(val) {
-        return () => {
-            let columnsCopy = this.props.columns.map((a) => ({...a, show: a.required || val}));
-            this.changeColumns(columnsCopy);
-        };
-    }
+  render() {
+    let x = 0
 
-    render() {
-        var x = 0;
-
-        return <Popover style={{ width: 340 }} button={<button aria-label="Choose Columns" className={"btn btn-sm ml-2 " + (this.props.buttonStyle || "btn") + "-primary"}><i className="fa fa-plus"></i></button>}>
-            <span onClick={this.setAllColumns(true)} className={"btn mb-2 mr-2 btn-sm " + (this.props.buttonStyle || "btn") + "-secondary"} style={{ width: 150, textAlign: 'center' }}>Select all</span>
-            <span onClick={this.setAllColumns(false)} className={"btn mb-2 btn-sm " + (this.props.buttonStyle || "btn") + "-secondary"} style={{ width: 150, textAlign: 'center' }}>Clear</span>
-            <hr />
-            {this.props.columns.map((item, i) => {
-                if (item.required) {
-                    return false;
-                }
-                x += 1;
-                return <span key={i} onClick={this.handleToggle(i)} style={{ width: 150, textAlign: 'center' }} className={"btn mt-2 btn-sm " + (x % 2 === 1 ? "mr-2 " : "") + (this.props.buttonStyle || "btn") + (item.show ? '-primary' : '-secondary')}>{item.Header}</span>;
-            })}
-        </Popover>;
-    }
+    return (
+      <Popover style={{ width: 340 }} button={<button aria-label='Choose Columns' className={`btn btn-sm ml-2 ${this.props.buttonStyle || 'btn'}-primary`}><i className='fa fa-plus' /></button>}>
+        <span onClick={this.setAllColumns(true)} className={`btn mb-2 mr-2 btn-sm ${this.props.buttonStyle || 'btn'}-secondary`} style={{ width: 150, textAlign: 'center' }}>Select all</span>
+        <span onClick={this.setAllColumns(false)} className={`btn mb-2 btn-sm ${this.props.buttonStyle || 'btn'}-secondary`} style={{ width: 150, textAlign: 'center' }}>Clear</span>
+        <hr />
+        {this.props.columns.map((item, i) => {
+          if (item.required) {
+            return false
+          }
+          x += 1
+          return <span key={i} onClick={this.handleToggle(i)} style={{ width: 150, textAlign: 'center' }} className={`btn mt-2 btn-sm ${x % 2 === 1 ? 'mr-2 ' : ''}${this.props.buttonStyle || 'btn'}${item.show ? '-primary' : '-secondary'}`}>{item.Header}</span>
+        })}
+      </Popover>
+    )
+  }
 }
 
-
-export default ColumnSelector;
+export default ColumnSelector
