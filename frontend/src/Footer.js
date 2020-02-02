ca94c060e5c89a1872a4406b91f044a8cb8fac0c
Daniel Tao <dtao@seas.upenn.edu>
2020-02-02 17:46:45 -0500

    format Footer

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -8,24 +8,24 @@ import { get_logout_url } from './api'
 const Footer = ({ style }) => (
   <div style={style} id='footer'>
     <div id='footer-inner'>
-      <Link to='/about'>About</Link>
-      {' '}|{' '}
-      <Link to='/faq'>FAQs</Link>
-      {' '}|{' '}
-      <a target='_blank' rel='noopener noreferrer' href='https://airtable.com/shrVygSaHDL6BswfT'>Feedback</a>
-      {' '}|{' '}
-      <a href={get_logout_url()}>Logout</a>
-
+      <Link to='/about'>About</Link> | <Link to='/faq'>FAQs</Link> |{' '}
+      <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://airtable.com/shrVygSaHDL6BswfT'
+      >
+        Feedback
+      </a>{' '}
+      | <a href={get_logout_url()}>Logout</a>
       <p id='copyright'>
-        Made with
-        {' '}
-        <i style={{ color: '#F56F71' }} className='fa fa-heart' />
-        {' '}by{' '}
-        <a href='https://pennlabs.org'><strong>Penn Labs</strong></a>
-        {' '}|{' '}
-        Hosted by
-        {' '}
-        <a href='https://stwing.upenn.edu/'><strong>STWing</strong></a>
+        Made with <i style={{ color: '#F56F71' }} className='fa fa-heart' /> by{' '}
+        <a href='https://pennlabs.org'>
+          <strong>Penn Labs</strong>
+        </a>{' '}
+        | Hosted by{' '}
+        <a href='https://stwing.upenn.edu/'>
+          <strong>STWing</strong>
+        </a>
       </p>
     </div>
   </div>
