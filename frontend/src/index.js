1636db3c93efaa905d20a4df79f4e517a48680f0
Daniel Tao <dtao@seas.upenn.edu>
2020-02-02 17:46:29 -0500

    format index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,17 +10,23 @@ import AuthPage from './AuthPage'
 import GoogleAnalytics from './GoogleAnalytics'
 
 if (window.location.hostname !== 'localhost') {
-  window.Raven.config('https://1eab3b29efe0416fa948c7cd23ed930a@sentry.pennlabs.org/5').install()
+  window.Raven.config(
+    'https://1eab3b29efe0416fa948c7cd23ed930a@sentry.pennlabs.org/5',
+  ).install()
 }
 
-ReactDOM.render((
+ReactDOM.render(
   <Router>
     <Switch>
       <Route exact path='/' component={ReviewPage} />
-      <Route path='/:type(course|department|instructor)/:code' component={AuthPage} />
+      <Route
+        path='/:type(course|department|instructor)/:code'
+        component={AuthPage}
+      />
       <Route path='/:page(about|faq|cart)' component={InfoPage} />
       <Route component={InfoPage} />
     </Switch>
     <GoogleAnalytics />
-  </Router>
-), document.getElementById('root'))
+  </Router>,
+  document.getElementById('root'),
+)
