bf1844745a4fc4ed8db769d1bef341d68138d2cb
Daniel Tao <dtao@seas.upenn.edu>
2020-02-25 18:08:46 -0500

    rename from Component -> WrappedComponent

--- frontend/src/pages/withLayout.js
+++ frontend/src/pages/withLayout.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
-export default Component => props => (
+export default WrappedComponent => props => (
   <>
     <Navbar />
-    <Component {...props} />
+    <WrappedComponent {...props} />
     <Footer />
   </>
 )
