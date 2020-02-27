7ac00c9a5a4d040f715113cd089c1f80a79f31e6
Daniel Tao <dtao@seas.upenn.edu>
2020-02-26 21:57:58 -0500

    alphabetize imports

--- frontend/src/DetailsBox.js
+++ frontend/src/DetailsBox.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, forwardRef } from 'react'
+import React, { forwardRef, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import ScoreTable from './ScoreTable'
