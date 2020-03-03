6ab56be1d67c875f6bd577aca5e9a435b2df11a8
Daniel Tao <dtao@seas.upenn.edu>
2020-03-03 00:05:50 -0500

    extract out DepartmentInfo table component

---
+++ frontend/src/components/InfoBox/DepartmentInfo.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Bar } from 'react-chartjs-2'
+
+import { getColumnName } from '../../utils/helpers'
+
+const chartColorMap = {
+  rCourseQuality: '#6274f1',
+  rInstructorQuality: '#ffc107',
+  rDifficulty: '#76bf96',
+  rWorkRequired: '#df5d56',
+}
+
+const CHART_COLUMNS = [
+  'rCourseQuality',
+  'rInstructorQuality',
+  'rDifficulty',
+  'rWorkRequired',
+]
+
+const generateChartData = courses => {
+  return {
+    labels: Object.values(courses).map(({ original: { code } }) => code),
+    datasets: CHART_COLUMNS.map(column => ({
+      label: getColumnName(column),
+      data: Object.values(courses).map(
+        ({
+          original: {
+            [column]: { average = 0 },
+          },
+        }) => average
+      ),
+      backgroundColor: chartColorMap[column],
+    })),
+  }
+}
+
+const chartOptions = {
+  scales: {
+    yAxes: [
+      {
+        display: true,
+        ticks: {
+          min: 0,
+          max: 4,
+        },
+      },
+    ],
+  },
+}
+
+export default ({ courses }) => {
+  const chartData =
+    courses && Object.keys(courses).length && generateChartData(courses)
+  return (
+    <div className="department-content">
+      {chartData ? (
+        <div id="row-select-chart-container">
+          <Bar data={chartData} options={chartOptions} />
+        </div>
+      ) : (
+        <div id="row-select-placeholder">
+          <object type="image/svg+xml" data="/static/image/selectrow.svg">
+            <img alt="Select Row" src="/static/image/selectrow.svg" />
+          </object>
+          <div id="row-select-text">
+            Select a few rows to begin comparing courses.
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
