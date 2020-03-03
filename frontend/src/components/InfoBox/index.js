4f3d808b19d17d0fb4e4118978b90911e8abb85f
Daniel Tao <dtao@seas.upenn.edu>
2020-03-03 01:03:09 -0500

    refactor index using new components

--- frontend/src/components/InfoBox/index.js
+++ frontend/src/components/InfoBox/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import reactStringReplace from 'react-string-replace'
 
 import Ratings from './InfoRatings'
 import { CourseDescription, CourseHeader } from './CourseInfo'
+import InstructorInfo from './InstructorInfo'
+import { DepartmentHeader, DepartmentGraphs } from './DepartmentInfo'
 import { apiContact } from '../../utils/api'
 
 /**
@@ -33,12 +33,26 @@ const InfoBox = ({
   const [inCourseCart, setInCourseCart] = useState(
     Boolean(localStorage.getItem(code))
   )
+  const {
+    rInstructorQuality: avgInstructorQuality,
+    rCourseQuality: avgCourseQuality,
+    rDifficulty: avgDifficulty,
+  } = average
+  const {
+    rInstructorQuality: recentInstructorQuality,
+    rCourseQuality: recentCourseQuality,
+    rDifficulty: recentDifficulty,
+  } = recent
+
+  const isCourse = type === 'course'
+  const isInstructor = type === 'instructor'
+  const isDepartment = type === 'department'
 
   useEffect(() => {
-    if (type === 'instructor') apiContact(name).then(setContact)
+    if (isInstructor) apiContact(name).then(setContact)
   }, [name, type])
 
-  const handleAdd = key => {
+  const handleCartAdd = key => {
     let instructor = 'Average Professor'
     if (key !== 'average') {
       ;({
@@ -62,22 +76,11 @@ const InfoBox = ({
     if (window.onCartUpdated) window.onCartUpdated()
     setInCourseCart(true)
   }
-  const handleRemove = () => {
+  const handleCartRemove = () => {
     localStorage.removeItem(code)
     setInCourseCart(false)
     if (window.onCartUpdated) window.onCartUpdated()
   }
-
-  const {
-    rInstructorQuality: avgInstructorQuality,
-    rCourseQuality: avgCourseQuality,
-    rDifficulty: avgDifficulty,
-  } = average
-  const {
-    rInstructorQuality: recentInstructorQuality,
-    rCourseQuality: recentCourseQuality,
-    rDifficulty: recentDifficulty,
-  } = recent
 
   if (!data) {
     return <h1>Loading data...</h1>
@@ -86,131 +89,29 @@ const InfoBox = ({
   return (
     <div className="box">
       <div id="banner-info" data-type="course">
-        {type === 'course' && (
-          <div className="course">
-            <div className="title">
-              {code.replace('-', ' ')}
-
-              <span className="float-right">
-                {inCourseCart ? (
-                  <span
-                    onClick={handleRemove}
-                    className="courseCart btn btn-action"
-                    title="Remove from Cart"
-                  >
-                    <i className="fa fa-fw fa-trash-alt" />
-                  </span>
-                ) : (
-                  <Popover
-                    button={
-                      <span
-                        className="courseCart btn btn-action"
-                        title="Add to Cart"
-                      >
-                        <i className="fa fa-fw fa-cart-plus" />
-                      </span>
-                    }
-                  >
-                    <div className="popover-title">Add to Cart</div>
-                    <div
-                      className="popover-content"
-                      style={{ maxHeight: 400, overflowY: 'auto' }}
-                    >
-                      <div id="divList">
-                        <ul className="professorList">
-                          <li>
-                            <button onClick={() => handleAdd('average')}>
-                              Average Professor
-                            </button>
-                          </li>
-                          {Object.keys(instructors)
-                            .sort((a, b) =>
-                              instructors[a].name.localeCompare(
-                                instructors[b].name
-                              )
-                            )
-                            .map(key => (
-                              <li key={key}>
-                                <button onClick={() => handleAdd(key)}>
-                                  {instructors[key].name}
-                                </button>
-                              </li>
-                            ))}
-                        </ul>
-                      </div>
-                    </div>
-                  </Popover>
-                )}{' '}
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  title="Get Alerted"
-                  href={`https://penncoursealert.com/?course=${code}&source=pcr`}
-                  className="btn btn-action"
-                >
-                  <i className="fas fa-fw fa-bell" />
-                </a>
-              </span>
-            </div>
-            {Boolean(aliases.length) && (
-              <div className="crosslist">
-                Also:{' '}
-                {aliases.map((cls, i) => [
-                  i > 0 && ', ',
-                  <Link key={cls} to={`/course/${cls}`}>
-                    {cls}
-                  </Link>,
-                ])}
-              </div>
-            )}
-            <p className="subtitle">{name}</p>
-            {notes.map(note => (
-              <div key={note} className="note">
-                <i className="fa fa-thumbtack" /> {note}
-              </div>
-            ))}
-            {type === 'course' && liveData && (
-              <Tags
-                {...liveData}
-                data={data}
-                existingInstructors={Object.values(instructors).map(
-                  a => a.name
-                )}
-              />
-            )}
-          </div>
+        {isCourse && (
+          <CourseHeader
+            aliases={aliases}
+            code={code}
+            name={name}
+            notes={notes}
+            instructors={instructors}
+            type={type}
+            inCourseCart={inCourseCart}
+            handleAdd={handleCartAdd}
+            handleRemove={handleCartRemove}
+            liveData={liveData}
+          />
         )}
 
-        {type === 'instructor' && (
-          <div className="instructor">
-            <div className="title">{name}</div>
-            {contact && (
-              <div>
-                <p className="desc">
-                  Email:{' '}
-                  <a href={`mailto:${contact.email}`}>
-                    {contact.email.toLowerCase()}
-                  </a>
-                </p>
-              </div>
-            )}
-            {notes.map(note => (
-              <div key={note} className="note">
-                <i className="fa fa-thumbtack" /> {note}
-              </div>
-            ))}
-          </div>
+        {isInstructor && (
+          <InstructorInfo name={name} contact={contact} notes={notes} />
         )}
 
-        {type === 'department' && (
-          <div className="department">
-            <div className="title">{name}</div>
-            <p className="subtitle">{code}</p>
-          </div>
-        )}
+        {isDepartment && <DepartmentHeader name={name} code={code} />}
       </div>
 
-      {type !== 'department' && (
+      {!isDepartment && (
         <div id="banner-score">
           <Ratings
             value="Average"
@@ -230,17 +131,9 @@ const InfoBox = ({
         </div>
       )}
 
-      {type === 'department' && <DepartmentInfo courses={selectedCourses} />}
+      {isDepartment && <DepartmentGraphs courses={selectedCourses} />}
 
-      {type === 'course' && (
-        <p className="desc">
-          {reactStringReplace(description, /([A-Z]{2,4}[ -]\d{3})/g, (m, i) => (
-            <Link to={`/course/${m.replace(' ', '-')}`} key={m + i}>
-              {m}
-            </Link>
-          ))}
-        </p>
-      )}
+      {isCourse && <CourseDescription description={description} />}
     </div>
   )
 }
